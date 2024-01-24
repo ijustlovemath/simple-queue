d383e3fa19e0c99275fd32080dec5e27a6fa424d
Jeremy DeJournett <jeremy@idealmedtech.com>
2024-01-24 10:54:40 -0500

    growing the vector and peeking the queue. lifetime management likely to change

--- sq.c
+++ sq.c
@@ -12,10 +12,12 @@
 #define SQ_BASE_CAPACITY (256) // make sure that you fit in a cache line to start!
 #endif
 
+#define qstorage_grow(T) qstorage_ ## T ## _grow
+#define qstorage_decl(T) struct queue_storage_ ## T
+
 #define define_queue(T) \
 struct queue_storage_ ## T { 	\
 	T* data;		\
-	size_t elem_size;	\
 	size_t max_capacity;	\
 	size_t capacity;	\
 	int64_t tip;		\
@@ -43,10 +45,10 @@ int qstorage_ ## T ## _init(struct queue_storage_ ## T **details, size_t max_cap
 		printf("res->data=%p, inicap=%zu, elsz=%zu\n", (void*)res->data, initial_capacity, elem_size);				\
 		goto cleanup_res;				\
 	}							\
-	res->elem_size = elem_size;				\
 	res->max_capacity = max_capacity;			\
 	res->capacity = initial_capacity;			\
 	res->tip = -1;						\
+	*details = res;						\
 	goto done;						\
 cleanup_res:							\
 	free(res);						\
@@ -62,7 +64,7 @@ int qstorage_ ## T ## _destroy(struct queue_storage_ ## T **details)\
 		status = -EINVAL;\
 		goto done;\
 	}\
-	struct queue_storage_ ## T *d = *details;\
+	qstorage_decl(T) *d = *details;\
 	if(!d) {\
 		status = -EINVAL;\
 		goto null_out;\
@@ -75,12 +77,45 @@ done:\
 	return status;\
 \
 }\
+int qstorage_ ## T ## _grow(qstorage_decl(T) *details) \
+{\
+	int status = 0;\
+	size_t max_capacity = details->max_capacity;\
+	size_t new_capacity = details->capacity;\
+	if(max_capacity == 0) {\
+		max_capacity = SIZE_MAX;\
+	}\
+	if(new_capacity > SIZE_MAX >> 1) {\
+		new_capacity = SIZE_MAX;\
+		goto cap;\
+	}\
+	new_capacity >>= 1;\
+	goto cap;\
+cap:\
+	if(new_capacity > max_capacity) {\
+		new_capacity = max_capacity;\
+	}\
+	/*if we ever get a capacity > SIZE_MAX / sizeof(T), we're in trouble. unlikely to ever happen in production */\
+	details->data = realloc(details->data, sizeof(T) * new_capacity);\
+	if(!details->data) {\
+		status = -ENOMEM;\
+		goto done;\
+	}\
+	details->capacity = new_capacity;\
+\
+done:\
+	return status;\
+}\
 \
 int qstorage_ ## T ## _append(struct queue_storage_ ## T *details, T *elem)\
 {\
 	int status = 0;\
 	details->tip++;\
 	/* TODO: check if we need to increase the capacity.. */\
+	if(details->tip >= details->capacity) {\
+		status = qstorage_grow(T)(details);\
+	}\
+	/* TODO Use this memcpy approach or let user manage element lifetime? */\
 	details->data[details->tip] = *elem;\
 	return status;\
 }\
@@ -129,6 +164,19 @@ int sq_enqueue_ ## T (struct simple_queue_ ## T *q, T *elem)\
 	/* unlock the mutex */\
 	return status;\
 }\
+int sq_peek_ ## T (sq_declare(T) *q, T **peek)\
+{\
+	if(!q || !q->details || !q->details->data || !peek){\
+		*peek = NULL;\
+		return -EINVAL;\
+	}\
+	if(q->details->tip < 0) {\
+		*peek = NULL;\
+		return 0;\
+	}\
+	*peek = &q->details->data[q->details->tip];\
+	return 0;\
+}\
 
 struct element {
 	int flags;
@@ -140,12 +188,25 @@ typedef struct element elem_s;
 
 #define sq_init(T) sq_ ## T ## _init
 
+#define sq_enqueue(T) sq_enqueue_ ## T
+
+#define sq_peek(T) sq_peek_ ## T
+
 define_queue(elem_s)
 
 int main(void)
 {
 	sq_declare(elem_s) *q;
 	int res = sq_init(elem_s)(&q, 1000, 0);
+	elem_s x = {.flags=1};
+	res = sq_enqueue(elem_s)(q, &x);
+	elem_s *peek = NULL;
+	res = sq_peek(elem_s)(q, &peek);
+	if(peek) {
+		printf("peek->flags=%d\n", peek->flags);
+	} else {
+		puts("peek failed to find anything");
+	}
 	printf("res=%d\n", res);
 	printf("simple calloc: %p\n", calloc(1, 1));
 }
